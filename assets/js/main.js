a1bb4d3786b5346ebf95949eb0a222b6ffe51d10
ex-jandal <512.jandal@gmail.com>
2024-07-20 10:17:35 +0300

    added the new section and blur to the header

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,15 @@ const linkAction = () =>{
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+/*=============== add blur to the header ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
 // /*=============== CHANGE BACKGROUND HEADER ===============*/
 // const scrollHeader = () =>{
 //     const header = document.getElementById('header')
